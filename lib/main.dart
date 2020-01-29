657a065de087844a484455a8d32552db2f947aaf
Douglas Barbosa <douglas_b@live.com>
2020-01-29 12:04:38 -0300

    Adicionar arquivos do aplicativo.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:aplicativo_gif/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+    
